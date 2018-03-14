5a001f72b850a54c33504b214bf243952b4e7660
knod <knodding@gmail.com>
2018-03-14 17:27:46 -0400

    Adds `journeys` object

---
+++ src/journeys/journeys.js
@@ -0,0 +1,14 @@
+// For designers to edit
+
+export default journeys = [
+// Copy and paste this, brackets and all, and put in the information you want
+// Items will appear in the order in which they are written in here
+
+    {
+        "name": "useful_name_for_designers",
+        "iconFileName": "icon-or-image-name.png",
+        "uri": "http://youtube.address.com"
+    },
+
+
+];  // end journeys
